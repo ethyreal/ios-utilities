c2e8f4a29575174b8f9b4c905944f35414ce38ee
George Webster <accounts@ethyreal.com>
2015-05-04 17:19:59 -0700

    added test project to run tests on

---
+++ tests/ios-utilities/ios-utilities-tests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tealium iOS Utilites
+//
+//  Created by George Webster on 5/4/15.
+//  Copyright (c) 2015 Tealium Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tests/ios-utilities/ios-utilities-tests/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tealium iOS Utilites
+//
+//  Created by George Webster on 5/4/15.
+//  Copyright (c) 2015 Tealium Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
