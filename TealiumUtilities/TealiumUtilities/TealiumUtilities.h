b5e06a06846bf7971aabae92128d0c529b5117e1
George Webster <accounts@ethyreal.com>
2015-05-08 13:32:20 -0700

    static library build

---
+++ TealiumUtilities/TealiumUtilities/TealiumUtilities.h
@@ -0,0 +1,13 @@
+//
+//  TealiumUtilities.h
+//  TealiumUtilities
+//
+//  Created by George Webster on 5/8/15.
+//  Copyright (c) 2015 Tealium Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TealiumUtilities : NSObject
+
+@end
---
+++ TealiumUtilities/TealiumUtilities/TealiumUtilities.m
@@ -0,0 +1,13 @@
+//
+//  TealiumUtilities.m
+//  TealiumUtilities
+//
+//  Created by George Webster on 5/8/15.
+//  Copyright (c) 2015 Tealium Inc. All rights reserved.
+//
+
+#import "TealiumUtilities.h"
+
+@implementation TealiumUtilities
+
+@end
