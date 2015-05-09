b014865c92702b93da0dc8a3df62f18c69837a39
George Webster <accounts@ethyreal.com>
2015-05-09 12:39:29 -0700

    fixed library compile added framework build

---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by George Webster on 5/8/15.
+//  Copyright (c) 2015 George Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
